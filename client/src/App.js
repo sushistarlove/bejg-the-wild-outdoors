4f9a7deb33ca2cdb42d0f16ce4966d4b9ec10a4c
GitPit <rifflifter@gmail.com>
2021-08-14 13:28:56 -0400

    imported seven Components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,5 +8,13 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Explore from './Components/Explore';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import ViewPost from './Components/ViewPost';
+import EditPost from './Components/EditPost';
+import Comment from './Components/Comment';
+
 
 export default App;
