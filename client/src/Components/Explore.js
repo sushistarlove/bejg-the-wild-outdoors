178b4ad37fe48a4e1c732e6ac589db525075296d
GitPit <rifflifter@gmail.com>
2021-08-14 17:29:37 -0400

    refactored Explore from mockup into Ben's code

--- client/src/Components/Explore.js
+++ client/src/Components/Explore.js
@@ -6,8 +6,53 @@ const Explore = () => {
   // const { loading, data } = useQuery(QUERY_POSTS, {
   //   fetchPolicy: "no-cache"
 
+    const postList = data?.posts || [];
+
+
+
+  return (
+
+<div class="container">
+
+{loading ? (<div>Loading...</div>) : (
+  <div class="columns">
+{postList.map((post) => {
+            return (
+  <div key={post._id} class="column-is-three-quarters" style="padding-left: 10px;">
+      <div class="columns is-multiline">
+
+      {/* <!-- card start --> */}
+   <div class="column is-half-desktop is-half-tablet is-full-mobile" style="background-color: #FFEFD3;">
+    <div class="card">
+     <div class="card-content">
+         <div class="content">
+           <h2 class="postTitle" style="text-align: center">A Kayaker's Guide to Vancouver Island</h2>
+           Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at
+           eros. Donec id elit non mi porta gravida at eget metus. Cum
+           sociis natoque penatibus et magnis dis parturient montes,
+           nascetur ridiculus mus. Cras mattis consectetur purus sit amet
+           fermentum.
+         </div>
+         <div class="icons">
+          <i class="fas fa-anchor"></i>
+          <i class="fas fa-campground"></i>
+          <i class="fas fa-fish"></i>
+          <i class="fas fa-binoculars"></i>
+         </div> 
+       </div>
+    </div>
+   </div>
+{/* <!-- card end --> */}
+</div>
+</div>)
+</div>
+}
+  </div>
+
+  );
+
   
-  };
+ ) };
 
 
 //i think this mapping logic would be more applicable to the ViewPost component rather than the Explore
