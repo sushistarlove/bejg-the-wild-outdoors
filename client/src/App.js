4a8c0839b44b5b84ad4b05e5a71f3f5b6e3ddb19
GitPit <rifflifter@gmail.com>
2021-08-14 13:31:25 -0400

    set Consts for GraphQL, auth, Apollo in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,4 +17,32 @@ import EditPost from './Components/EditPost';
 import Comment from './Components/Comment';
 
 
+//GraphQL code from Unit 21 Act 26
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//authLink middleware code from Unit 21 Act 26
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+//Apollo Client code from Unit 21 Act 26
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 export default App;
