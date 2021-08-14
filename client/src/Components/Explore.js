cce44ca021672bc09236e3592cd2a64ac1254561
GitPit <rifflifter@gmail.com>
2021-08-14 17:17:53 -0400

    commented out Explore.js function

--- client/src/Components/Explore.js
+++ client/src/Components/Explore.js
@@ -3,42 +3,44 @@ import { useQuery } from '@apollo/client';
 import { QUERY_POSTS } from '../utils/queries';
 
 const Explore = () => {
-  const { loading, data } = useQuery(QUERY_POSTS, {
-    fetchPolicy: "no-cache"
-  });
+  // const { loading, data } = useQuery(QUERY_POSTS, {
+  //   fetchPolicy: "no-cache"
+
+  
+  };
 
 
 //i think this mapping logic would be more applicable to the ViewPost component rather than the Explore
 
-  const postList = data?.posts || [];
+//   const postList = data?.posts || [];
 
-  return (
-    <div className="column is-10 is-offset-1">
+//   return (
+//     <div className="column is-10 is-offset-1">
 
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="columns deck">
-          {postList.map((post) => {
-            return (
-              <div key={post._id} class="card column is-5">
-                <div class="card-content">
-                  <div class="content">
-                  <Link to={{ pathname: `/post/${post._id}` }}>
+//       {loading ? (
+//         <div>Loading...</div>
+//       ) : (
+//         <div className="columns deck">
+//           {postList.map((post) => {
+//             return (
+//               <div key={post._id} class="card column is-5">
+//                 <div class="card-content">
+//                   <div class="content">
+//                   <Link to={{ pathname: `/post/${post._id}` }}>
                     
-                    <h2>{post.postTitle}</h2>
-                  </Link>
+//                     <h2>{post.postTitle}</h2>
+//                   </Link>
 
-                  </div>
-                </div>
-              </div>
+//                   </div>
+//                 </div>
+//               </div>
 
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
-};
+//             );
+//           })}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
 export default Explore;
