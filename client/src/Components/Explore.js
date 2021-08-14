7c27e77131cb8e25520b97d35a8e42089e20d5b5
GitPit <rifflifter@gmail.com>
2021-08-14 17:49:19 -0400

    replaced Explore.js logic/ added syntax err!!!

--- client/src/Components/Explore.js
+++ client/src/Components/Explore.js
@@ -8,20 +8,18 @@ const Explore = () => {
 
     const postList = data?.posts || [];
 
-
-
   return (
 
 <div className="container">
 
-{loading ? (<div>Loading...</div>) : (
+{ loading ? (<div>Loading...</div>) : (
   <div className="columns">
-{postList.map((post) => {
+{ postList.map((post) => {
             return (
   <div key={post._id} className="column-is-three-quarters" style={{paddingLeft: "10px"}}>
       <div class="columns is-multiline">
 
-      {/* <!-- card start --> */}
+     
    <div className="column is-half-desktop is-half-tablet is-full-mobile" style={{backgroundColor: "#FFEFD3"}}>
     <div className="card">
      <div className="card-content">
@@ -38,50 +36,21 @@ const Explore = () => {
        </div>
     </div>
    </div>
-{/* <!-- card end --> */}
+
+
 </div>
-</div>)
+</div>)}
+
+)
+)
+            }
 </div>
 }
-  </div>)
-
-  
-
-  
- ) };
+</div>
+  )
+  }
+  };
 
 
-{/* //i think this mapping logic would be more applicable to the ViewPost component rather than the Explore
-
-//   const postList = data?.posts || [];
-
-//   return (
-//     <div className="column is-10 is-offset-1">
-
-//       {loading ? ( */}
-{/* //         <div>Loading...</div>
-//       ) : (
-//         <div className="columns deck">
-//           {postList.map((post) => { */}
-{/* //             return (
-//               <div key={post._id} class="card column is-5">
-//                 <div class="card-content">
-//                   <div class="content">
-//                   <Link to={{ pathname: `/post/${post._id}` }}>
-                    
-//                     <h2>{post.postTitle}</h2>
-//                   </Link>
-
-//                   </div>
-//                 </div>
-//               </div>
-
-//             );
-//           })}
-//         </div>
-//       )}
-//     </div>
-//   );
-// }; */}
 
 export default Explore;
