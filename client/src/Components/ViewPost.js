4caed1ed9d274cc577dde4e15716e9787a036078
GitPit <rifflifter@gmail.com>
2021-08-14 15:04:58 -0400

    wrote draft of responsive ViewPost Card and css

--- client/src/Components/ViewPost.js
+++ client/src/Components/ViewPost.js
@@ -22,9 +22,44 @@ if (loading) {
 
 return (
 
+    <div className="container">
+
+<div class="image">
+    <img src="" alt="">
+    <div class="details">
+        <div class="content">
+                  <h2 className="postTitle" style={{ textAlign: "center" }}>Welcome to Wild Outdoors</h2>
+                  Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at
+                  eros. Donec id elit non mi porta gravida at eget metus. Cum
+                  sociis natoque penatibus et magnis dis parturient montes,
+                  nascetur ridiculus mus. Cras mattis consectetur purus sit amet
+                  fermentum.
+                </div>
+        <div class="more">
+            <a href="" target="blank"
+                rel="noopener noreferrer" class="read-more">View<span>Post</span></a>
+            
+            <div class="icons">
+                 <i class="fas fa-campground"></i>
+                 <i class="fas fa-hiking"></i>
+                 <i class="fas fa-fish"></i>
+                 <i class="fas fa-snowboarding"></i>
+                 <i class="fas fa-binoculars"></i>
+                 <i class="fas fa-anchor"></i>
+                 <i class="fas fa-mountain"></i>
+                </div> 
 
 
-    // <Comment />
+            </div>
+        </div>
+    
+</img>
+    </div>
+    </div>
+   
+
+    // {/* // <Comment /> */}
+
 );
 };
 
