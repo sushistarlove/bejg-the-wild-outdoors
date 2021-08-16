1ef566bb950203015b5f5c58e4127f516b948543
GitPit <rifflifter@gmail.com>
2021-08-16 14:21:32 -0400

    re-removed dummy text from ViewPost

--- client/src/Components/ViewPost.js
+++ client/src/Components/ViewPost.js
@@ -46,11 +46,7 @@ return (
                 </div>
 
                 <div className="postContent">
-                Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at
-                  eros. Donec id elit non mi porta gravida at eget metus. Cum
-                  sociis natoque penatibus et magnis dis parturient montes,
-                  nascetur ridiculus mus. Cras mattis consectetur purus sit amet
-                  fermentum.
+                
                 </div>
             
             <div className="icons">
