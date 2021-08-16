a819c429d451f2403d839ae618f93fba4c96dfa2
Jared Colletti <JaredColletti123@gmail.com>
2021-08-16 09:56:28 -0400

    commit

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -4,39 +4,43 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => { //complete
-   
-      return User.find().populate('posts');
+    users: async () => {
+      //complete
+
+      return User.find().populate("posts");
     },
 
-    user: async (parent, { username }) => { //complete
+    user: async (parent, { username }) => {
+      //complete
 
       return User.findOne({ username: username });
     },
-    posts: async (parent, { userID }) => { //complete
-      if(!userID) {
+    posts: async (parent, { userID }) => {
+      //complete
+      if (!userID) {
         return Post.find().sort({ createdAt: -1 });
-
       } else {
-        return Post.find({user_id: userID}).sort({ createdAt: -1 });
+        return Post.find({ user_id: userID }).sort({ createdAt: -1 });
       }
-      
     },
-    post: async (parent, { postId }) => { //complete
+    post: async (parent, { postId }) => {
+      //complete
       //good
       return Post.findOne({ _id: postId });
     },
   },
 
   Mutation: {
-    addUser: async (parent, { username, password }) => { //complete
+    addUser: async (parent, { username, password }) => {
+      //complete
       //good
       const user = await User.create({ username, password });
       const token = signToken(user);
 
       return { token, user };
     },
-    login: async (parent, { username, password }) => { //complete
+    login: async (parent, { username, password }) => {
+      //complete
       //good
       const user = await User.findOne({ username });
 
@@ -54,70 +58,78 @@ const resolvers = {
       return { token, user };
     },
 
-    addPost: async (parent, { postTitle, postContent }, context) => { //maybe
-      //good
+    addPost: async (parent, { postTitle, postContent, tag }, context) => {
+      //complete
       if (context.user) {
+        let newTag = {
+          category: tag
+        }
+        console.log(context.user);
+        console.log(newTag);
         const post = await Post.create({
           postTitle,
           postContent,
+          postAuthor: context.user.username,
+          user_id: context.user._id,
+          categories: newTag ,
         });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { posts: post._id } }
-        );
-
         return post;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
 
     removePost: async (parent, { postId }, context) => {
+      //complete
       if (context.user) {
         const post = await Post.findOneAndDelete({
           _id: postId,
         });
 
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { posts: post._id } } //good
-        );
-
         return post;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
-    editPost: async (parent, { postTitle, postContent, postId }, context) => { //good
- 
+    editPost: async (parent, { postTitle, postContent, postId }, context) => {
+      //complete
+
       if (context.user) {
         const post = await Post.findOneAndUpdate(
           { _id: postId },
-          { postTitle, postContent }
-        );
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { posts: post._id } }
+          { postTitle: postTitle, postContent: postContent }
         );
 
         return post;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
-    addComment: async (parent, { postId, comment }) => { //good
-      return Post.findOneAndUpdate(
-        { _id: postId },
-        {
-          $addToSet: { comments: comment },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
+    addComment: async (
+      parent,
+      { postId, commentText },
+      context
+    ) => {
+      //complete
+
+      if (context.user) {
+        let newComment = {
+          commentText: commentText,
+          commentAuthor: context.user.username,
+        };
+        console.log(newComment);
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $addToSet: { comments: newComment },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      } else {
+        throw new AuthenticationError("You need to be logged in!");
+      }
     },
   },
 };
 
 module.exports = resolvers;
-
