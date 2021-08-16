57863c75133ba1c41824bb603ebc3ce4918545e1
GitPit <rifflifter@gmail.com>
2021-08-16 10:46:52 -0400

    added dropdown menu to CreatePost

--- client/src/Components/CreatePost.js
+++ client/src/Components/CreatePost.js
@@ -6,11 +6,21 @@ const CreatePost = () => {
         <div className="container">
          <div className="column is-three-quarters-desktop is-full-mobile">
 
-         <input class="input" type="text" placeholder="Post Title">
+         <input class="input" type="text" placeholder="Post Title"></input>
 
          <textarea class="textarea" placeholder="Post Content" rows="50"></textarea>
 
-
+         <div class="select">
+  <select>
+    <option>Select Categories</option>
+    <option>Boating</option>
+    <option>Camping</option>
+    <option>Climbing</option>
+    <option>Fishing</option>
+    <option>Hiking</option>
+    <option>Hunting</option>
+  </select>
+</div>
 
             </div>
         </div>
