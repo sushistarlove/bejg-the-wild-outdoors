4da5395a631ce746892408ffe9888f0b33636410
Ben Thackray <mthack92@gmail.com>
2021-08-16 13:37:04 -0400

    updated navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -11,26 +11,153 @@ library.add(fab, faHiking, faFish, faCampground, faBinoculars);
 
 
 
-const Navbar = () => {
-    return (
+// const Navbar = () => {
+//     return (
 
-        <aside  className="menu column-is-one-quarter is-mobile">
-        <p className="menu-label">Categories</p>
+//         <aside  className="menu column-is-one-quarter is-mobile">
+//         <p className="menu-label">Categories</p>
+//         <ul className="menu-list">
+//           <li><Link>Hiking    <FontAwesomeIcon icon="faHiking" /></Link> </li>
+//           <li><Link>Fishing    <FontAwesomeIcon icon="faFish" /></Link></li>
+//           <li><Link>Camping    <FontAwesomeIcon icon="faCampground" /></Link></li>
+//           <li><Link>Misc    <FontAwesomeIcon icon="faBinoculars" /></Link></li>
+//         </ul>
+//         <p className="menu-label">Profile</p>
+//         <ul className="menu-list">
+//           <li><Link>My Posts</Link></li>
+//           <li a="/login"><Link>Login</Link> </li>
+//           <li><Link>Account Settings</Link></li>
+//         </ul>
+//       </aside>
+
+//     )
+// };
+
+// export default Navbar;
+
+function Navbar({ currentPage, handlePageChange }) {
+  return (
+    <aside className="menu column-is-one-quarter is-mobile">
+      {/* <p className="menu-label">Categories</p>
         <ul className="menu-list">
           <li><Link>Hiking    <FontAwesomeIcon icon="faHiking" /></Link> </li>
           <li><Link>Fishing    <FontAwesomeIcon icon="faFish" /></Link></li>
           <li><Link>Camping    <FontAwesomeIcon icon="faCampground" /></Link></li>
           <li><Link>Misc    <FontAwesomeIcon icon="faBinoculars" /></Link></li>
-        </ul>
-        <p className="menu-label">Profile</p>
-        <ul className="menu-list">
-          <li><Link>My Posts</Link></li>
-       
-          <li><Link>Account Settings</Link></li>
-        </ul>
-      </aside>
+        </ul> */}
+      <p className="menu-label">Profile</p>
+      <ul className="menu-list">
+        {/* <li><Link>My Posts</Link></li> */}
 
-    )
-};
+        <li className="nav-item">
+          <a
+            href="/"
+            onClick={() => handlePageChange('Explore')}
+            className={currentPage === 'Explore' ? 'nav-link active' : 'nav-link'}
+          >
+            Explore
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a
+            href="signup"
+            onClick={() => handlePageChange('Signup')}
+            className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
+          >
+            Signup
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a
+            href="login"
+            onClick={() => handlePageChange('Login')}
+            className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
+          >
+            Login
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a
+            href="createpost"
+            onClick={() => handlePageChange('Create Post')}
+            className={currentPage === 'Create Post' ? 'nav-link active' : 'nav-link'}
+          >
+            Create Post
+          </a>
+        </li>
+
+
+        {/* <li className="nav-item">
+          <a
+            href="viewpost"
+            onClick={() => handlePageChange('My Posts')}
+            className={currentPage === 'My Posts' ? 'nav-link active' : 'nav-link'}
+          >
+            My Posts
+          </a>
+        </li> */}
+
+        {/* <li><Link>Account Settings</Link></li> */}
+
+      </ul>
+    </aside>
+
+
+
+    // <div className="menu column-is-one-quarter is-mobile">
+
+    //   <div className="name">
+    //     <h1><span id="ben">Ben</span>Thackray</h1>
+    //   </div>
+    //   <ul className="nav nav-tabs">
+    //     <li className="nav-item">
+    //       <a
+    //         href="login"
+    //         onClick={() => handlePageChange('Home')}
+    //         className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+    //       >
+    //         Home
+    //       </a>
+    //     </li>
+    //     <li className="nav-item">
+    //       <a
+    //         href="#about"
+    //         onClick={() => handlePageChange('About')}
+    //         //  TODO: Add a comment explaining what this logic is doing
+
+    //         className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+    //       >
+    //         Resume
+    //       </a>
+    //     </li>
+    //     <li className="nav-item">
+    //       <a
+    //         href="#blog"
+    //         onClick={() => handlePageChange('Blog')}
+    //         //  TODO: Add a comment explaining what this logic is doing
+
+    //         className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+    //       >
+    //         Portfolio
+    //       </a>
+    //     </li>
+    //     <li className="nav-item">
+    //       <a
+    //         href="#contact"
+    //         //  TODO: Add a comment explaining what this logic is doing
+
+    //         onClick={() => handlePageChange('Contact')}
+    //         className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+    //       >
+    //         Contact
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </div>
+  );
+}
 
 export default Navbar;
