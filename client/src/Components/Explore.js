fd1dd5f2a4807070832e0fd1be75236426b60b2d
gunnar ulatowski <gunnarulatowski@outlook.com>
2021-08-16 21:40:02 -0400

    C:/Program Files/Git/

--- client/src/Components/Explore.js
+++ client/src/Components/Explore.js
@@ -1,10 +1,24 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_POSTS } from '../utils/queries';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faHiking, faFish, faCampground, faBinoculars, faAnchor } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_POSTS } from "../utils/queries";
+import './Explore.css';
+import hiking from '../images/hiking1.jpg';
+import hiking2 from '../images/hiking2.jpg';
+import camp from '../images/camping1.jpg';
+import kayak from '../images/kayak1.jpg';
+import climb from '../images/climbing1.jpg';
+import fish from '../images/fishing1.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import Auth from "../utils/auth";
+import {
+  faHiking,
+  faFish,
+  faCampground,
+  faBinoculars,
+  faAnchor,
+} from "@fortawesome/free-solid-svg-icons";
 
 
 library.add(fab, faHiking, faFish, faCampground, faBinoculars, faAnchor);
